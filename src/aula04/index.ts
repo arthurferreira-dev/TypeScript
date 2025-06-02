48c292ad6a07a499925f282c144d902e5ea608dc
Arthur Ferreira <tutudacosta996@gmail.com>
2025-06-02 17:30:54 -0300

    aula03

---
+++ src/aula04/index.ts
@@ -0,0 +1,8 @@
+// type annotations
+
+const String: string = 'Arthur Ferreira Dev'
+const Number: number = Infinity
+const Boolean: boolean = false
+const Qualquer: any = {}
+const Nulo: null = null
+const Indefinido: undefined = undefined
